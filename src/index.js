7b1d1396a572f672a004384f7f25eff5a6c05671
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-01 11:25:01 -0500

    Finish basic first component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 // import App from './App';
 import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Hello />
+    <Hello greeting={'Welcome to React.'}/>
   </React.StrictMode>
 );
 
